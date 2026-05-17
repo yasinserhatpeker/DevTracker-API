607ee3e5314d026a26c14a0ab8b5dba9c86027b8
Yasin <yasinpeker43@gmail.com>
2026-05-17 10:34:31 +0300

    Project and Task model is created

--- devtracker/models.py
+++ devtracker/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    
+    title = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return (f"{self.title} - {'is finished' if self.is_completed else 'is pending...'}")
+
+
+    
