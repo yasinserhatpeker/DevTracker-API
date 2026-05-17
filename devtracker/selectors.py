fb60df6f81dd1b34793553f9db4901f92f03faf3
Yasin <yasinpeker43@gmail.com>
2026-05-17 10:48:02 +0300

    selectors file is done

--- devtracker/selectors.py
+++ devtracker/selectors.py
@@ -1,5 +1,15 @@
 from django.db.models import QuerySet
 from .models import Project, Task
 
+def get_projects() -> QuerySet[Project]:
+    return Project.objects.prefetch_related('tasks').all() # avoiding n+1 problem with prefetch_related
+
+
+def get_project_by_id(*,project_id:int) -> Project | None:
+    return Project.objects.filter(id=project_id).first() # fetching related project
+
+
+def get_tasks_by_project(*,project_id:int) -> QuerySet[Task]:
+    return Task.objects.filter(project_id=project_id).first()  # fetching tasks by a project
 
 
