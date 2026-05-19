bf9f8259c2598797f316065104cc9bb4e33e31e5
Yasin <yasinpeker43@gmail.com>
2026-05-19 13:13:16 +0300

    fix typo

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "rest_framework_simplejwt",
-    "rest_framework_simplejwt.token_blacklist"
+    "rest_framework_simplejwt.token_blacklist",
     "devtracker",
     
 ]
