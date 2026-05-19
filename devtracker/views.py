21ce3e650030e087ed074cfb8f695b5924a716cc
Yasin <yasinpeker43@gmail.com>
2026-05-19 12:57:39 +0300

    logout endpoint is done

--- devtracker/views.py
+++ devtracker/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.exceptions import ValidationError
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from . import selectors,services
 from .serializers import (
@@ -186,7 +187,18 @@ class LogoutApi(APIView):
     """
     Route: /api/user/register/
     """
-        
-    
+    def post(self,request):
+       try:
+           refresh_token = request.data.get('refresh')
+           if not refresh_token:
+              return Response({"detail":"Refresh token is mandatory"}, status=status.HTTP_400_BAD_REQUEST)
+          
+           token = RefreshToken(refresh_token)
+           token.blacklist()
+           
+           return Response({"detail":"Logout successfully"},status=status.HTTP_205_RESET_CONTENT)
+       
+       except Exception:
+           return Response({"detail":"Invalid token:"},status=status.HTTP_400_BAD_REQUEST)   
     
         
